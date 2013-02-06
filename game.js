4199499939fae9866303dd2554d6070a98c8b095
Brendon John <contact@brendonjohn.com>
2013-02-06 17:16:35 +1300

    Init
    learning how to work with the html5 canvas

---
+++ game.js
@@ -0,0 +1,64 @@
+var DIRECTIONS = {
+	left:[-5,0],
+	right:[5,0],
+	up:[0,-5],
+	down:[0,5]
+};
+
+var context;
+
+KeyboardJS.on('up', function() {
+	console.log('pressed: Up');
+	MoveCharacter(DIRECTIONS['up']);
+});
+
+KeyboardJS.on('down', function() {
+	console.log('pressed: down');
+	MoveCharacter(DIRECTIONS['down']);
+});
+
+KeyboardJS.on('left', function() {
+	console.log('pressed: left');
+	MoveCharacter(DIRECTIONS['left']);
+});
+
+KeyboardJS.on('right', function() {
+	console.log('pressed: right');
+	MoveCharacter(DIRECTIONS['right']);
+});
+
+var character = {
+	x:10,
+	y:10,
+	height: 90,
+	width: 90
+};
+
+$(document).ready(function(){
+	context = document.getElementById('arena').getContext('2d');
+	DrawCharacter();
+	
+});
+
+function DrawCharacter(){
+    context.fillRect (
+    	character['x'],
+    	character['y'],
+    	character['width'],
+    	character['height']
+    );
+	
+}
+
+function MoveCharacter(direction){
+	context.clearRect(
+		character['x'],
+    	character['y'],
+    	character['width'],
+    	character['height']
+	);
+	
+	character['x'] = character['x'] + direction[0];
+	character['y'] = character['y'] + direction[1];
+	DrawCharacter();
+}
